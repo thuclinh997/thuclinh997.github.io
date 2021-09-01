3f048531a17c9b251a5da204dfa08e08689b9a4d
thuclinh997 <caothuclinh997@gmail.com>
2021-09-01 16:41:44 +0700

    update 02

---
+++ javascript/facebook/1.js
@@ -0,0 +1,33 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    //code...
+    //khai bao cac bien su dung
+    let ndHienRa = document.getElementsByClassName('dehienthi');
+    let nut = document.getElementsByClassName('declick');
+    let trangThai = 1;
+    for (var i = 0; i < nut.length; i++) {
+        nut[i].onclick = function () {
+            // console.log(this.classList[1]);
+            if (this.classList[1] == 'mau-trang') {
+                // console.log('day la truong hop con lai');
+                this.classList.remove('mau-trang');
+                let hienThi = this.getAttribute('data-hienthi');
+                let phanTuHienRa = document.getElementById(hienThi);
+                phanTuHienRa.classList.toggle('ra');
+
+            } else {
+                for (var j = 0; j < nut.length; j++) {
+                    nut[j].classList.remove('mau-trang');
+                }
+                this.classList.toggle('mau-trang');
+                //lay ve data-hienthi
+                let hienThi = this.getAttribute('data-hienthi');
+                let phanTuHienRa = document.getElementById(hienThi);
+                // console.log(phanTuHienRa);
+                for (var i = 0; i < ndHienRa.length; i++) {
+                    ndHienRa[i].classList.remove('ra');
+                }
+                phanTuHienRa.classList.toggle('ra');
+            }
+        }
+    }
+}, false)
---
+++ javascript/menu/1.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    //code...
+    let nut = document.querySelector('.nut'),
+        menuTrai = document.querySelector('.menutrai'),
+        den = document.querySelector('.den'),
+        noiDungTo = document.querySelector('.noidungto'),
+        nut2 = document.querySelector('.nut2'),
+        trang = document.querySelector('.mautrang');
+    // xu ly su kien click vao nut
+    nut.onclick = function () {
+        den.classList.add('len');
+        noiDungTo.classList.add('dichphai');
+    }
+    nut2.onclick = function () {
+        den.classList.add('len');
+        menuTrai.classList.remove('vetrai');
+        trang.classList.add('trangquay');
+    }
+    den.onclick = function () {
+        this.classList.remove('len');
+        menuTrai.classList.add('vetrai');
+        noiDungTo.classList.remove('dichphai');
+        trang.classList.remove('trangquay');
+
+    }
+}, false)
